f372c7d60e22c6e64532d0930a0b98d032a5087a
howsun <howsun7@gmail.com>
2022-05-23 00:29:11 -0700

    add links app

---
+++ links/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django_extensions.db.models import TimeStampedModel
+
+
+class Category(TimeStampedModel):
+    name = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name
+
+class Link(TimeStampedModel):
+    url = models.URLField()
+    times_saved = models.PositiveIntegerField()
+
+
